3ebc51bd9e46a8b451ee21e74077112c27afa93f
Steve Hall <steve@mowgli.local>
2018-09-27 13:48:20 +0300

    initial version

---
+++ TedTheRoboCollector/Assets/scripts/Collector.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// A collecting game object
+/// </summary>
+public class Collector : MonoBehaviour
+{
+	#region Fields
+
+    // targeting support
+    SortedList<Target> targets = new SortedList<Target>();
+    Target targetPickup = null;
+
+    // movement support
+	const float BaseImpulseForceMagnitude = 2.0f;
+    const float ImpulseForceIncrement = 0.3f;
+	
+	// saved for efficiency
+    Rigidbody2D rb2d;
+
+    private float currentClosestDistance = float.MaxValue;
+
+    // circling behaviour
+    private bool circling;
+    private float rotateSpeed = 5f;
+    private float radius = 0.01f;
+    private Vector2 centre;
+    private float angle;
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+    {
+		// center collector in screen
+		Vector3 position = transform.position;
+		position.x = 0;
+		position.y = 0;
+		position.z = 0;
+		transform.position = position;
+
+		// save reference for efficiency
+		rb2d = GetComponent<Rigidbody2D>();
+
+        // add as listener for pickup spawned event
+        EventManager.AddListener(AddToList);
+	}
+
+    void Update()
+    {
+        if (circling)
+        {
+            transform.position = getCirclingPosition();
+        }
+    }
+
+    private void startCircling()
+    {
+        centre = transform.position;
+        circling = true;
+        radius = 0.01f;
+    }
+
+    private Vector3 getCirclingPosition()
+    {
+        angle += rotateSpeed * Time.deltaTime;
+
+        var offset = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)) * radius;
+        return centre + offset;
+    }
+
+    void FixedUpdate()
+    {
+        if (radius < 0.5f)
+            radius += 0.001f;
+    }
+
+    /// <summary>
+    /// Called when another object is within a trigger collider
+    /// attached to this object
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (targets.Count > 0)
+        {
+            // only respond if the collision is with the target pickup
+            if (other.gameObject == targetPickup.GameObject)
+            {
+                // remove collected pickup from list of targets and game
+                int index = targets.IndexOf(targetPickup);
+                targets.RemoveAt(index);
+                Destroy(targetPickup.GameObject);
+
+                // update distances for all targets
+                int count = targets.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    targets[i].UpdateDistance(transform.position);
+                }
+                targets.Sort();
+
+                // go to next target if there is one
+                if (targets.Count > 0)
+                {
+                    SetTarget(targets[count - 1]);
+                    circling = false;
+                }
+                else
+                    startCircling();
+            }
+        } else {
+            SetTarget(null);
+        }
+    }
+
+    void AddToList(GameObject gameObjectToAdd)
+    {
+        Target target = new Target(gameObjectToAdd, transform.position);
+        targets.Add(target);
+
+        if (currentClosestDistance > target.Distance)
+        {
+            currentClosestDistance = target.Distance;
+            SetTarget(target);
+        }
+    }
+
+	/// <summary>
+	/// Sets the target pickup to the provided pickup
+	/// </summary>
+	/// <param name="pickup">Pickup.</param>
+	void SetTarget(Target pickup)
+    {
+        targetPickup = pickup;
+
+        if (targetPickup != null)
+            GoToTargetPickup();
+    }
+
+    /// <summary>
+    /// Starts the teddy bear moving toward the target pickup
+    /// </summary>
+    void GoToTargetPickup()
+    {
+        if (targetPickup != null)
+        {
+            int numOfPicks = GameObject.FindGameObjectsWithTag("Pickup").Length;
+
+            // calculate direction to target pickup and start moving toward it
+            Vector2 direction = new Vector2(
+                targetPickup.GameObject.transform.position.x - transform.position.x,
+                targetPickup.GameObject.transform.position.y - transform.position.y);
+            direction.Normalize();
+            rb2d.velocity = Vector2.zero;
+            Vector2 force = direction * BaseImpulseForceMagnitude * numOfPicks;
+            rb2d.AddForce(force,
+                ForceMode2D.Impulse);
+        }
+    }
+
+    #endregion
+}
---
+++ TedTheRoboCollector/Assets/scripts/SortedList.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// A sorted list
+/// </summary>
+public class SortedList<T> where T:IComparable
+{
+    List<T> items = new List<T>();
+
+    // used in Add method
+    List<T> tempList = new List<T>();
+	
+    #region Constructors
+
+    /// <summary>
+    /// No argument constructor
+    /// </summary>
+    public SortedList()
+    {
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the number of items in the list
+    /// </summary>
+    /// <value>number of items in the list</value>
+    public int Count
+    {
+        get { return items.Count; }
+    }
+	
+    /// <summary>
+    /// Gets the item in the array at the given index
+    /// This property allows access using [ and ]
+    /// </summary>
+    /// <param name="index">index of item</param>
+    /// <returns>item at the given index</returns>
+    public T this[int index]
+    {
+        get { return items[index]; }
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Adds the given item to the list
+    /// </summary>
+    /// <param name="item">item</param>
+    public void Add(T itemToAdd)
+    {
+        // get number of items already in list
+        int count = items.Count; // O(1)
+
+        if (count == 0)
+        {
+            items.Add(itemToAdd);
+        } 
+        else
+        {
+            // in worst case scenario, we will be inserting at end of list
+            int index = count; // O(1)
+
+            // iterate over items and identify insertion index for new item
+            for (int i = 0; i < count; i++) // O(n)
+            {
+                T item = items[i]; // O(1)
+                if (itemToAdd.CompareTo(item) < 0) // O(1)
+                {
+                    // if itemToAdd is greater than item, we want to insert
+                    // itemToAdd at index of item
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index >= count)
+            {
+                items.Add(itemToAdd); // O(n)
+            }
+            else
+            {
+                // save all items beyond the insertion index in a temporary list
+                tempList = items.GetRange(index, count - index); // O(n)
+
+                // remove all items beyond the insertion point
+                items.RemoveRange(index, count - index); // O(n)
+
+                // add the new item at the end of the list
+                items.Add(itemToAdd); // O(n)
+
+                // add back in all the items in the temporary list
+                items.AddRange(tempList); // O(n)
+            }
+
+            // be nice and clear the temp list
+            tempList.Clear(); // O(n)
+        }
+    }
+
+    /// <summary>
+    /// Removes the item at the given index from the list
+    /// </summary>
+    /// <param name="index">index</param>
+    public void RemoveAt(int index)
+    {
+        // add your implementation below
+        items.RemoveAt(index);
+    }
+
+    /// <summary>
+    /// Determines the index of the given item using binary search
+    /// </summary>
+    /// <param name="item">the item to find</param>
+    /// <returns>the index of the item or -1 if it's not found</returns>
+    public int IndexOf(T item)
+    {
+        int lowerBound = 0;
+        int upperBound = items.Count - 1;
+        int location = -1;
+
+        // if last item is still the closest (which it might well be)
+        // skip the searching
+        bool skip = false;
+        T lastValue = items[upperBound];
+        if (lastValue.CompareTo(item) == 0)
+        {
+            location = upperBound;
+            skip = true;
+        }
+
+        if (!skip)
+        {
+            // loop until found value or exhausted array
+            while ((location == -1) &&
+                (lowerBound <= upperBound))
+            {
+                // find the middle
+                int middleLocation = lowerBound + (upperBound - lowerBound) / 2;
+                T middleValue = items[middleLocation];
+
+                // check for match
+                if (middleValue.CompareTo(item) == 0)
+                {
+                    location = middleLocation;
+                }
+                else
+                {
+                    // split data set to search appropriate side
+                    if (middleValue.CompareTo(item) > 0) // middleValue is smaller than item
+                    {
+                        upperBound = middleLocation - 1;
+                    }
+                    else // middleValue is smaller than item
+                    {
+                        lowerBound = middleLocation + 1;
+                    }
+                }
+            }
+        }
+
+        return location;
+    }
+
+    /// <summary>
+    /// Sorts the list
+    /// </summary>
+    public void Sort()
+    {
+        items.Sort();
+    }
+
+    #endregion
+}
